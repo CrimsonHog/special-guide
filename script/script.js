2d9c6fdeefffd6037080d909dd8c1b7fb9700507
CrimsonHog <111534189+CrimsonHog@users.noreply.github.com>
2022-09-19 19:27:59 -0500

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,44 @@
+$(function() {
+    console.log("document is ready!");
+    var userName, nameLength, nameMsg, nameReverse = ""; 
+  
+    $('#btnName').click(function() {
+        userName = $('#userName').val(); 
+        nameMsg = "Your spirit animal is: ";
+        $('#nameOutput').append(nameMsg + nameReverse + "<br/>"); 
+        console.log(thirdLetter(userName)); 
+        stringArrayPosition(userName); 
+        });
+
+        $('#btnClear').click(function(){
+            clearElement("#nameOutput", "html");
+            clearElement("#userName", "input");})
+       function getStringLength(stringVal){
+       var stringLength = stringVal.length; 
+       return stringLength; 
+    }
+
+    function reverseString(stringVal){
+        var revString = stringVal.split("").reverse().join("");
+       return revString; 
+    }
+  
+    function thirdLetter(stringVal){
+       return thirdString; var thirdString = stringVal[2]; 
+    }
+
+    function clearElement(elementId, elementType){
+        if(elementType == "html"){
+            $(elementId).html(""); 
+        }
+        else if(elementType=="input"){
+            $(elementId).val(" "); 
+        }
+    } 
+  
+    function stringArrayPosition(stringVal){
+        for(i=0; i<stringVal.length; i++){
+            console.log(i + " " + stringVal[i]);
+        }
+    }
+});
